937dc6d2ba4fc7136bf6b5f8cbc01f2e17c646d1
yakir9970 <yakirafeka@gmail.com>
2022-09-21 20:32:30 +0300

    Added some userList logic

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './UserItem.css';
+
+const UsersItem = (props) => {
+
+};
+
+export default UsersItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return (
-        <h2>Users Works!</h2>
-    );
+  const USERS = [
+    {
+      id: "u1",
+      name: "max",
+      image:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&psig=AOvVaw0CyRWxjjEdtF9sCcJqOxtI&ust=1663867052095000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOCoovexpvoCFQAAAAAdAAAAABAD",
+      places: 3,
+    },
+  ]; //dummy data
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
