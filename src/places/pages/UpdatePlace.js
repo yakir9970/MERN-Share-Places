305fa99cf1db65fbba03d28b493e4d076a6ded75
yakir9970 <yakirafeka@gmail.com>
2022-09-26 18:43:40 +0300

    Added Update Place page and custom Form Hook

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import "./PlaceForm.css";
+
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+
+const DUMMY = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky acrapers in the world!",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "350 Fifth Avenue · Manhattan, New York 10118",
+    location: {
+      lat: 40.748441,
+      lng: -73.985664,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky acrapers in the world!",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "350 Fifth Avenue · Manhattan, New York 10118",
+    location: {
+      lat: 40.748441,
+      lng: -73.985664,
+    },
+    creator: "u2",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY.find((p) => p.id === placeId);
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title!"
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description! (min 5 characters)"
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
