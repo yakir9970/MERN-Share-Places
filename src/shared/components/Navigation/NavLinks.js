93c6ae3dff3761e2807364d5f87a28e6b2f02044
yakir9970 <yakirafeka@gmail.com>
2022-10-08 20:29:56 +0300

    FINISHED!

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -27,7 +27,7 @@ const NavLinks = (props) => {
       )}
       {!auth.isLoggedIn && (
         <li>
-          <NavLink to="/auth">AUTHENTICATE</NavLink>
+          <NavLink to="/auth">LOGIN</NavLink>
         </li>
       )}
       {auth.isLoggedIn && (
